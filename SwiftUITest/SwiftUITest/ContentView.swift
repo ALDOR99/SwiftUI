95be49e9eaec63423c2a943d603965b6d0bd8267
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2024-01-09 21:34:06 +0300

    swiftUI

---
+++ SwiftUITest/SwiftUITest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Ali Durna on 9.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("HELLOWORLD")
+                }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITest/SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Ali Durna on 9.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
